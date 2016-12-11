c8f5ad0cf52da8f2d4f9681b38ac76ac185205e6
Funkahdafi <ldm242@gmail.com>
2016-12-11 03:14:30 +0100

    Update: Console-Output, HTML-Output

--- RPI-vHost.sh
+++ RPI-vHost.sh
@@ -2,10 +2,17 @@
 
 #########################################################################################
 #											#
-# Author: Tom Baumbach									#
+# Programm ausführen:									#
+# -------------------									#
+# sudo chmod +x ./RPI-vHost.sh								#
+# sudo ./RPI-vHost.sh									#
+#											#
+# Zusätzliche Informationen:								#
+# --------------------------								#
+# Autor	: Tom Baumbach									#
 # GitHub: https://github.com/Funkahdafi							#
 #											#
-# RPI-vhost.sh wurde für Rasbian 8 entwickelt und ist OpenSource.			#
+# RPI-vHost.sh wurde für Rasbian 8 entwickelt und ist OpenSource.			#
 # Für andere Linux-Distributionen sind Anpassungen am Skript notwendig.			#
 #											#
 # Für Schäden, die durch diese Software entstehen können, übernehme ich keine Haftung.	#
@@ -18,17 +25,17 @@ clear;												#Bildschirm löschen
 
 apache_dir=/etc/apache2										#Apache2 Installationspfad
 
-html_dir=/var/www/html										#Apache2 DocumentRoot, innerhalb dessen wird vhost angelegt.
+html_dir=/var/www/html										#Apache2 DocumentRoot für Ihren vHost.
 
-index_file=index.html										#Index z.B.: *.htm, *.html, *.php, *.php5
+index_file=index.html										#Index-Datei: .htm, .html, .php, .php5
 
-server_admin_mail=mail@example.com								#E-Mail des Serveradmin
+server_admin_mail=mail@example.com								#E-Mail Serveradmin
 
-domain=localhost										#Auf welcher Domain soll der vhost laufen ?
+domain=localhost										#Auf welcher Domain soll der vHost laufen ?
 
 #ENDE KONFIGURATION
 
-if [[ $EUID != 0 ]];										#Script muss als Root ausgeführt werden !
+if [[ $EUID != 0 ]];										#Check: Root
 then
 	echo "";
 	echo -e "\e[41mBitte führen Sie das Script als Root aus !\e[49m";
@@ -36,10 +43,11 @@ then
 	exit
 else
 	echo "";
-        echo "Willkommen beim Apache2 Virtual Host-Generator.";
+        echo "Willkommen bei RPI-vHost - Apache2 vHost Generator.";
+	echo "---------------------------------------------------";
         echo "";
 
-if [ ! -d $apache_dir ];									#Ist Apache2 vorhanden ?
+if [ ! -d $apache_dir ];									#Check: Apache2
 then
 	echo -e "\e[41m$apache_dir ist nicht vorhanden!\e[49m";
 	echo "Das Programm wird beendet! Bitte installieren Sie Apache2.";
@@ -47,26 +55,26 @@ then
 	exit
 else
 
-if [ ! -d $apache_dir/logs ];									#Apache2 log prüfen
+if [ ! -d $apache_dir/logs ];									#Check: Apache2 log
 then
 	sudo mkdir $apache_dir/logs;
 else
 
-function vhost_hinzufuegen(){									#Funktion vhost_hinzufügen ANFANG
+function vhost_hinzufuegen(){									#vhost_hinzufügen ANFANG
         echo "";
-	echo "Installation von $vhostname beginnt.";
+	echo "Die Installation von vHost $vhostname beginnt.";
         echo "Dieser Vorgang kann einige Sekunden in Anspruch nehmen.";
 	echo "";
         sleep 1
         sudo mkdir $html_dir/$vhostname;
-        echo "Status: $html_dir/$vhostname wurde angelegt.";
+        echo -e "[ \033[32mok\033[0m ] $html_dir/$vhostname wurde angelegt.";
         sleep 1
         sudo chmod 755 $html_dir/$vhostname;
         sudo chown www-data:www-data $html_dir/$vhostname;
-        echo "Status: Schreibrecht für $html_dir/$vhostname gesetzt.";
+        echo -e "[ \033[32mok\033[0m ] Schreibrecht für $html_dir/$vhostname gesetzt.";
         sleep 1
         sudo touch $html_dir/$vhostname/$index_file;
-        echo "Status: $index_file wurde angelegt.";
+        echo -e "[ \033[32mok\033[0m ] $index_file wurde angelegt.";
         sleep 1
         sudo chmod 644 $html_dir/$vhostname/$index_file;
         sudo chown www-data:www-data $html_dir/$vhostname/$index_file;
@@ -81,40 +89,45 @@ function vhost_hinzufuegen(){									#Funktion vhost_hinzufügen ANFANG
         echo "  CustomLog logs/$vhostname.access_log common" >> $apache_dir/sites-available/$vhostname.conf;
         echo "</VirtualHost>" >> $apache_dir/sites-available/$vhostname.conf;
         sleep 1
-        echo "Status: $vhostname.conf wurde erfolgreich angelegt und wird aktiviert.";
+        echo -e "[ \033[32mok\033[0m ] $vhostname.conf wurde erfolgreich angelegt und wird aktiviert.";
         sleep 1
-        sudo a2ensite $vhostname.conf;
+        sudo a2ensite $vhostname.conf > /dev/null;
         sleep 1
-        echo "Status: Apache2 wird neugestartet.";
+	echo"";
+        echo "Apache2 wird neugestartet.";
 	echo "";
         sleep 1
-        sudo service apache2 reload;
+        sudo /etc/init.d/apache2 reload > /dev/null;
         sleep 3
-        echo "Ihr virtueller Host: $vhostname wurde erfolgreich in $html_dir angelegt." >> $html_dir/$vhostname/$index_file;
-	echo -e "Ihr  vhost ist unter \033[32mhttp://$vhostname.$domain\033[0m erreichbar.";
+	echo "<br>" >> $html_dir/$vhostname/$index_file;
+        echo "<center><h2>Ihr vHost: $vhostname wurde erfolgreich in $html_dir angelegt.</h2></center>" >> $html_dir/$vhostname/$index_file;
+	echo "<br><br>" >> $html_dir/$vhostname/$index_file;
+	echo "<center><a href=https://github.com/Funkahdafi/RPI-vHost target=_blank>RPI-vHost@GitHub</a></center>" >> $html_dir/$vhostname/$index_file;
+	echo -e "[ \033[32mok\033[0m ] Ihr  vHost ist unter \033[32mhttp://$vhostname.$domain\033[0m erreichbar.";
 	echo "";
         echo "Vielen Dank, wir sind fertig :-))";
+	echo "";
 	exit
-}												#Funktion vhost_hinzufügen ENDE
+}												#vhost_hinzufügen ENDE
 
-function vhost_eintragen(){									#Funtion vhost_eintragen ANFANG
-	echo "Bitte geben Sie einen neuen Hostnamen an:";
-	read hostname
-	vhostname=$(echo $hostname | tr '[A-Z]' '[a-z]');					#Eingabe to lower konvertieren
+function vhost_eintragen(){									#vhost_eintragen ANFANG
+	echo "Bitte geben Sie einen Hostnamen an.";
+	read -p "Eingabe: " hostname
+	vhostname=$(echo $hostname | tr '[A-Z]' '[a-z]');					#Eingabe to lower
 
-if [[ ! -e $apache_dir/sites-available/$vhostname.conf && ! -d $html_dir/$vhostname ]];		#Prüfung ob *.conf und directory vorhanden
+if [[ ! -e $apache_dir/sites-available/$vhostname.conf && ! -d $html_dir/$vhostname ]];		#Check: *.conf & dir
 then
 	echo "";
-	echo "$apache_dir/sites-available/$vhostname.conf ist frei.";
-	echo "$html_dir/$vhostname ist frei.";
+	echo -e "[ \033[32mok\033[0m ] $apache_dir/sites-available/$vhostname.conf ist frei.";
+	echo -e "[ \033[32mok\033[0m ] $html_dir/$vhostname ist frei.";
 	vhost_hinzufuegen
 else
 	echo "";
-	echo -e "Achtung: \e[41mvHostname $vhostname kann nicht vergeben werden.\e[49m !";
-	echo "Grund: Es gibt diesen vhost schon oder ein gleichnamiges HTML-Verzeichnis.";
+	echo -e "Achtung: \e[41mvHostname $vhostname kann nicht vergeben werden.\e[49m";
+	echo "Grund: Es gibt diesen vHost schon oder ein gleichnamiges HTML-Verzeichnis.";
 	echo "";
 	echo "Wollen Sie einen neuen Hostnamen wählen? j/n";
-	read antwort
+	read -p "Eingabe: " antwort
 	if [ $antwort = "j" ];
 then
 	vhost_eintragen
@@ -127,18 +140,17 @@ else
 	exit
 else
 	echo "";
-	echo "Ungültige Auswahl, probieren Sie bitte es erneut.";
+	echo "Ungültige Auswahl. Bitte probieren Sie es erneut.";
 	echo "";
 	vhost_eintragen
 fi
 fi
 fi
 
-}
+}												#vhost_eintragen ENDE
 
 	vhost_eintragen
 
 fi
 fi
 fi
-
